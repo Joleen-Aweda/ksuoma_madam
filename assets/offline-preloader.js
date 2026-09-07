d49c0827c8fcdb5fee4151ff07f2eaf417d665cd
Joleen-Aweda <joleenaweda@protonmail.com>
2026-09-07 16:32:55 +0300

    Bundle sign-language videos locally

--- assets/offline-preloader.js
+++ assets/offline-preloader.js
@@ -1,6 +1,6 @@
 // Load embedded data only for direct offline reading.
 (function(){
   if(window.location.protocol==="file:"){
-    document.write('<script src="./assets/offline-data.js?v=50"><\/script>');
+    document.write('<script src="./assets/offline-data.js?v=51"><\/script>');
   }
 })();
