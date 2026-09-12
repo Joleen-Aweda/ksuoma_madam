94ecae78d9c7425e7b8e0e5c3e3a6b2042b59ed8
Adolf <adolfalfred321@gmail.com>
2026-09-12 10:06:11 +0300

    feat: finalize responsive reader deployment

--- assets/offline-preloader.js
+++ assets/offline-preloader.js
@@ -1,6 +1,6 @@
 // Load embedded data only for direct offline reading.
 (function(){
   if(window.location.protocol==="file:"){
-    document.write('<script src="./assets/offline-data.js?v=33"><\/script>');
+    document.write('<script src="./assets/offline-data.js?v=35"><\/script>');
   }
 })();
